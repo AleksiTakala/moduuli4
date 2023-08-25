38b00a5779f6189ff5c95521529417e2a9fe144f
AleksiTakala <at.345688@gmail.com>
2023-08-25 12:56:37 +0300

    Tehtävä 6

---
+++ Tehtävä 6.py
@@ -0,0 +1,10 @@
+import math
+import random
+n=0
+N=int(input('Anna testattavien pisteiden määrä '))
+for i in range(N):
+    if pow(random.uniform(-1, 1), 2) + pow(random.uniform(-1, 1), 2) < 1:
+        n+=1
+print('Pi:n likiarvo on ' + str(4*n/N))
+
+
